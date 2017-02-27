f4021d37cc330ec4990f72d437ee4303d201193c
dong-alex <adong@ualberta.ca>
2017-02-27 13:26:28 -0700

    Participant

---
+++ app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/Participant.java
@@ -0,0 +1,40 @@
+package com.psychic_engine.cmput301w17t10.feelsappman;
+
+import java.util.ArrayList;
+
+/**
+ * Created by adong on 2/27/17.
+ */
+
+public class Participant {
+    public String login;
+    public ArrayList<MoodEvent> moodEvents;
+    public ArrayList<Participant> followers;
+    public ArrayList<Participant> following;
+    public ArrayList<Participant> pendingRequests;
+
+    public Participant(String loginName) {
+        this.login = loginName;
+        this.moodEvents = new ArrayList<MoodEvent>();
+        this.followers = new ArrayList<Participant>();
+        this.following = new ArrayList<Participant>();
+        this.pendingRequests = new ArrayList<Participant>();
+    }
+
+    public ArrayList<MoodEvent> getMoodList() {
+        return this.moodEvents;
+    }
+
+    public ArrayList<Participant> getFollowers() {
+        return this.followers;
+    }
+
+    public ArrayList<Participant> getFollowing() {
+        return this.following;
+    }
+
+    public ArrayList<Participant> getPendingRequests() {
+        return this.pendingRequests;
+    }
+}
+
