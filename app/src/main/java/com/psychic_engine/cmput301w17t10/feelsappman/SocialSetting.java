8eb8135f12b2e749b65a96026f9e941f7430e1f3
Jordi Pun <jordipun@yahoo.ca>
2017-02-27 15:02:50 -0700

    Participant test case complete

---
+++ app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/SocialSetting.java
@@ -0,0 +1,11 @@
+package com.psychic_engine.cmput301w17t10.feelsappman;
+
+/**
+ * Created by adong on 2/27/17.
+ */
+public enum SocialSetting {
+        alone(1),
+        oneother(2),
+        twotoseveral(3),
+        crowd(4);
+}
