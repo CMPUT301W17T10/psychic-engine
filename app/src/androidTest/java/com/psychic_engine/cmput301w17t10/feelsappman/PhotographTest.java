ca3408b26776ffbfbbeea210a1a0077e93302d48
Hussain Khan <hnkhan@cs.ualberta.ca>
2017-02-27 13:42:06 -0700

    PhotographTest incomplete

---
+++ app/src/androidTest/java/com/psychic_engine/cmput301w17t10/feelsappman/PhotographTest.java
@@ -0,0 +1,23 @@
+package com.psychic_engine.cmput301w17t10.feelsappman;
+
+/**
+ * Created by hnkhan on 2/27/17.
+ */
+
+public class PhotographTest {
+
+    public void testGetImage() {
+        //assume photograph exists
+
+    }
+
+    public void testSetImage() {
+        //assume photograph exists
+
+    }
+
+    public void testDeleteImage() {
+        //assume photograph exists
+
+    }
+}
