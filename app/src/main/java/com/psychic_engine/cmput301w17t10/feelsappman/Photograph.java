a3fd21d98e6556d9540063ead565c55fdeffbb95
Hussain Khan <hnkhan@cs.ualberta.ca>
2017-02-27 13:20:41 -0700

    Photograph.java skeleton

---
+++ app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/Photograph.java
@@ -0,0 +1,23 @@
+package com.psychic_engine.cmput301w17t10.feelsappman;
+
+/**
+ * Created by hnkhan on 2/27/17.
+ */
+
+public class Photograph {
+
+    private static Integer BYTE;
+    private Boolean limitSize;
+
+    public void getImage() {
+
+    }
+
+    public void setImage() {
+
+    }
+
+    public void deleteImage() {
+
+    }
+}
