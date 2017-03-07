cb710b88b4b233103fce5478b9e1b7bd6bab27fe
yuenj <yuenj@users.noreply.github.com>
2017-03-07 16:50:49 -0700

    Update CreateMoodActivity.java

--- app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/CreateMoodActivity.java
+++ app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/CreateMoodActivity.java
@@ -16,6 +16,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +36,7 @@ public class CreateMoodActivity extends AppCompatActivity {
     private Button browseButton;
     private ImageView photoImageView;
     private Button createButton;
+    private Button cancelButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,78 +45,50 @@ public class CreateMoodActivity extends AppCompatActivity {
 
         // set up mood and social setting spinners (drop downs)
         setUpSpinners();
-
-        //TODO not working perfectly - requires 2 clicks after initial click
-        // clear trigger edit text when user clicks in it if default msg is displayed
-        triggerEditText = (EditText) findViewById(R.id.trigger);
-        triggerEditText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (triggerEditText.getText().toString().equals(defaultTriggerMsg))
-                    triggerEditText.setText("");
-            }
-        });
-
-        // reset trigger edit text message if the user clicks away from it and it is blank
-        triggerEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
-                    // user has clicked out of triggerEditText
-                    if (triggerEditText.getText().toString().equals(""))
-                        triggerEditText.setText(defaultTriggerMsg);
-                }
-            }
-        });
-
-        // Taken from http://stackoverflow.com/questions/21072034/image-browse-button-in-android-activity
-        // on 03-06-17
-        browseButton = (Button) findViewById(R.id.browse);
-        browseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(
-                        Intent.ACTION_PICK,
-                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-                startActivityForResult(i, RESULT_LOAD_IMAGE);
-            }
-        });
-
-
-        createButton = (Button) findViewById(R.id.create);
-        createButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO (maybe?): createMoodController can observe when this button is pressed
-                // and perform below actions for this class
-                createMoodEvent();
-            }
-        });
+        // set up events that happen when user clicks in trigger and outside trigger
+        setUpTrigger();
+        // set up events that happen when user clicks browse button
+        setUpBrowse();
+        // set up events that happen when user clicks create button
+        setUpCreate();
+        // set up events that happen when user clicks cancel button
+        setUpCancel();
     }
 
-    // for displaying the browsed image
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+    void createMoodEvent() {
+        // get the mood from the mood spinner
+        String moodString = moodSpinner.getSelectedItem().toString();
+        Mood mood = null;
 
-            Cursor cursor = getContentResolver().query(selectedImage,
-                    filePathColumn, null, null, null);
-            cursor.moveToFirst();
-
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
-
-            photoImageView = (ImageView) findViewById(R.id.imageView);
-            photoImageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        switch(moodString) {        // TODO refactor this - inside MoodState enum class?
+            case "Sad": mood = new Mood(MoodState.SAD); break;
+            case "Happy": mood = new Mood(MoodState.HAPPY); break;
+            case "Shame": mood = new Mood(MoodState.SHAME); break;
+            case "Fear": mood = new Mood(MoodState.FEAR); break;
+            case "Anger": mood = new Mood(MoodState.ANGER); break;
+            case "Surprised": mood = new Mood(MoodState.SURPRISED); break;
+            case "Disgust": mood = new Mood(MoodState.DISGUST); break;
+            case "Confused": mood = new Mood(MoodState.CONFUSED); break;
+            default:
+                Toast.makeText(CreateMoodActivity.this,
+                        "Please specify a mood.",
+                        Toast.LENGTH_LONG).show();
+                return;
         }
-    }
 
+        // get the trigger from the trigger edit text
+        String trigger = triggerEditText.getText().toString();
+        if (trigger.equals(defaultTriggerMsg))
+            trigger = "";
+
+        Photograph photo = null; // TODO get photo from imageView but not sure what type it is
+        Location location = null; // TODO get location from location box - need to know how to use GOOGLE MAPS first
+
+        MoodEvent moodEvent = new MoodEvent(mood, trigger, photo, location);
+
+        // TODO need a class that keeps track of the current user so i can add this mood to that users mood list
+    }
 
     void setUpSpinners() {
         // Spinner elements
@@ -123,12 +97,14 @@ public class CreateMoodActivity extends AppCompatActivity {
 
         // Spinner drop down elements
         List<String> moodCategories = new ArrayList<String>();
+        moodCategories.add("");     // default option
         MoodState[] moodStates = MoodState.values();
         for (MoodState moodState : moodStates) {
             moodCategories.add(moodState.toString());
         }
 
         List<String> socialSettingCategories = new ArrayList<String>();
+        socialSettingCategories.add("");    // default option
         SocialSetting[] socialSettings = SocialSetting.values();
         for (SocialSetting socialSetting : socialSettings) {
             socialSettingCategories.add(socialSetting.toString());
@@ -151,33 +127,88 @@ public class CreateMoodActivity extends AppCompatActivity {
         socialSettingSpinner.setAdapter(socialSettingSpinnerAdapter);
     }
 
+    void setUpTrigger() {
+        //TODO not working perfectly - requires 2 clicks after initial click
+        // clear trigger edit text when user clicks in it if default msg is displayed
+        triggerEditText = (EditText) findViewById(R.id.trigger);
+        triggerEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (triggerEditText.getText().toString().equals(defaultTriggerMsg))
+                    triggerEditText.setText("");
+            }
+        });
 
-    void createMoodEvent() {
-        // get the mood from the mood spinner
-        String moodString = moodSpinner.getSelectedItem().toString();
-        Mood mood = new Mood(null);
+        // reset trigger edit text message if the user clicks away from it and it is blank
+        triggerEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    // user has clicked out of triggerEditText
+                    if (triggerEditText.getText().toString().equals(""))
+                        triggerEditText.setText(defaultTriggerMsg);
+                }
+            }
+        });
+    }
 
-        switch(moodString) {        // TODO refactor this - inside MoodState enum class?
-            case "Sad": mood.setMood(MoodState.SAD); break;
-            case "Happy": mood.setMood(MoodState.HAPPY); break;
-            case "Shame": mood.setMood(MoodState.SHAME); break;
-            case "Fear": mood.setMood(MoodState.FEAR); break;
-            case "Anger": mood.setMood(MoodState.ANGER); break;
-            case "Surprised": mood.setMood(MoodState.SURPRISED); break;
-            case "Disgust": mood.setMood(MoodState.DISGUST); break;
-            case "Confused": mood.setMood(MoodState.CONFUSED); break;
+    void setUpBrowse() {
+        // Taken from http://stackoverflow.com/questions/21072034/image-browse-button-in-android-activity
+        // on 03-06-17
+        browseButton = (Button) findViewById(R.id.browse);
+        browseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+    }
+
+    // displayed the browsed image
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            photoImageView = (ImageView) findViewById(R.id.imageView);
+            photoImageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
         }
+    }
 
-        // get the trigger from the trigger edit text
-        String trigger = triggerEditText.getText().toString();
-        if (trigger.equals(defaultTriggerMsg))
-            trigger = "";
+    void setUpCreate() {
+        createButton = (Button) findViewById(R.id.create);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO (maybe?): createMoodController can observe when this button is pressed
+                // and perform below actions for this class
+                createMoodEvent();
+            }
+        });
+    }
 
-        Photograph photo = null; // TODO get photo from imageView but not sure what type it is
-        Location location = null; // TODO get location from location box - need to know how to use GOOGLE MAPS first
-
-        MoodEvent moodEvent = new MoodEvent(mood, trigger, photo, location);
-
-        // TODO need a class that keeps track of the current user so i can add this mood to that users mood list
+    void setUpCancel() {
+        cancelButton = (Button) findViewById(R.id.cancel);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
