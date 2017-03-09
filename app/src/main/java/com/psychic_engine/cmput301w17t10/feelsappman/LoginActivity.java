c096601c3d433c6add5228ab9da10eb06487d1f1
dong-alex <adong@ualberta.ca>
2017-03-09 11:56:40 -0700

    typo

--- app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/LoginActivity.java
+++ app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/LoginActivity.java
@@ -23,7 +23,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.lang.reflect.Type;
-import java.util.ArrayList;a
+import java.util.ArrayList;
 
 // created by Alex Dong | March 6, 2017
 
