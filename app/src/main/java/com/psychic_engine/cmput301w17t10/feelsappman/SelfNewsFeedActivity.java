2fe366755f21ca4e90c29017df39e37e8e520331
dong-alex <adong@ualberta.ca>
2017-03-08 23:11:58 -0700

    SelfNewsFeedActivity

--- app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/SelfNewsFeedActivity.java
+++ app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/SelfNewsFeedActivity.java
@@ -3,6 +3,7 @@ package com.psychic_engine.cmput301w17t10.feelsappman;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -13,7 +14,7 @@ import java.util.ArrayList;
 //SelfNewsFeedActivity named, I dont know which activity it corresponds to in the UML -Alex
 
 public class SelfNewsFeedActivity extends AppCompatActivity {
-    private Participant selfParticipant;
+    //private Participant selfParticipant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +26,12 @@ public class SelfNewsFeedActivity extends AppCompatActivity {
         //startActivity(intent);
 
         Gson gsonIn = new Gson();
-        Intent intentIn = getIntent();
         Type listType = new TypeToken<ArrayList<Participant>>(){}.getType();
         String gsonObject = getIntent().getStringExtra("participantListObjects");
-        selfParticipant = gsonIn.fromJson(gsonObject, listType);
+        ArrayList<Participant> participantList = gsonIn.fromJson(gsonObject, listType);
+        for(Participant participant: participantList) {
+            Log.i("Participant", participant.getLogin());
+        }
 
     }
 }
