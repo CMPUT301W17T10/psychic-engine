8fa9eeb164969e7c8b05da2eca08a3b33a740ef0
yuenj <jyuen1@ualberta.ca>
2017-03-13 16:05:38 -0600

    recent mood

--- app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/EditMoodController.java
+++ app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/EditMoodController.java
@@ -10,68 +10,59 @@ import java.util.Date;
 
 public class EditMoodController {
 
-    public EditMoodController() {
+    static boolean updateMoodEventList(int moodEventPosition, String moodString, String socialSettingString, String trigger, Photograph photo, String location) {
+        Mood mood = null;
+        SocialSetting socialSetting;
 
-    }
-
-    static boolean updateMoodEventList(int moodEventPosition, String moodString, String socialSettingString, String trigger, Photograph photo, Location location) {
-        try {
-            Mood mood = null;
-            SocialSetting socialSetting;
-
-            switch (moodString) {        // TODO refactor this - inside MoodState enum class?
-                case "Sad":
-                    mood = new Mood(MoodState.SAD);
-                    break;
-                case "Happy":
-                    mood = new Mood(MoodState.HAPPY);
-                    break;
-                case "Shame":
-                    mood = new Mood(MoodState.SHAME);
-                    break;
-                case "Fear":
-                    mood = new Mood(MoodState.FEAR);
-                    break;
-                case "Anger":
-                    mood = new Mood(MoodState.ANGER);
-                    break;
-                case "Surprised":
-                    mood = new Mood(MoodState.SURPRISED);
-                    break;
-                case "Disgust":
-                    mood = new Mood(MoodState.DISGUST);
-                    break;
-                case "Confused":
-                    mood = new Mood(MoodState.CONFUSED);
-                    break;
-            }
-
-            switch (socialSettingString) {
-                case "Alone":
-                    socialSetting = SocialSetting.ALONE;
-                    break;
-                case "One Other":
-                    socialSetting = SocialSetting.ONEOTHER;
-                    break;
-                case "Two To Several":
-                    socialSetting = SocialSetting.TWOTOSEVERAL;
-                    break;
-                case "Crowd":
-                    socialSetting = SocialSetting.CROWD;
-                    break;
-                default:
-                    socialSetting = null;
-            }
-
-
-            MoodEvent moodEvent = new MoodEvent(mood, socialSetting, trigger, photo, null);
-
-            // replace old moodEvent with new one
-            ParticipantSingleton.getInstance().getSelfParticipant().setMoodEvent(moodEventPosition, moodEvent);
-            return true;
+        switch (moodString) {        // TODO refactor this - inside MoodState enum class?
+            case "Sad":
+                mood = new Mood(MoodState.SAD);
+                break;
+            case "Happy":
+                mood = new Mood(MoodState.HAPPY);
+                break;
+            case "Shame":
+                mood = new Mood(MoodState.SHAME);
+                break;
+            case "Fear":
+                mood = new Mood(MoodState.FEAR);
+                break;
+            case "Anger":
+                mood = new Mood(MoodState.ANGER);
+                break;
+            case "Surprised":
+                mood = new Mood(MoodState.SURPRISED);
+                break;
+            case "Disgust":
+                mood = new Mood(MoodState.DISGUST);
+                break;
+            case "Confused":
+                mood = new Mood(MoodState.CONFUSED);
+                break;
         }
-        catch (Throwable e) {
-            return false;
+
+        switch (socialSettingString) {
+            case "Alone":
+                socialSetting = SocialSetting.ALONE;
+                break;
+            case "One Other":
+                socialSetting = SocialSetting.ONEOTHER;
+                break;
+            case "Two To Several":
+                socialSetting = SocialSetting.TWOTOSEVERAL;
+                break;
+            case "Crowd":
+                socialSetting = SocialSetting.CROWD;
+                break;
+            default:
+                socialSetting = null;
         }
+
+
+        MoodEvent moodEvent = new MoodEvent(mood, socialSetting, trigger, photo, null);
+
+        // replace old moodEvent with new one
+        ParticipantSingleton.getInstance().getSelfParticipant().setMoodEvent(moodEventPosition, moodEvent);
+        return true;
     }
 }
