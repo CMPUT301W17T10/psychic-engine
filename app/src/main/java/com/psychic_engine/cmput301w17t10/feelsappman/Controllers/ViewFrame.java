cfc43ae6aca91b3167c3736cba8367c1b1c2b456
yuenj <jyuen1@ualberta.ca>
2017-03-15 12:16:54 -0600

    Created interface directory

--- app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/Controllers/ViewFrame.java
+++
@@ -1,9 +0,0 @@
-package com.psychic_engine.cmput301w17t10.feelsappman.Controllers;
-
-/**
- * Created by adong on 3/9/17.
- */
-
-public interface ViewFrame<M> {
-    public void update(M model);
-}
