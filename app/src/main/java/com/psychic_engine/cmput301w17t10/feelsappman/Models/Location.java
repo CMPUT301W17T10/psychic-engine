1bd0eac80c6756cca0599565a1d9c0d2dc7511cc
pslin1 <pslin@ualberta.ca>
2017-03-25 21:01:37 -0600

    Location class created, dependencies updated

---
+++ app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/Models/Location.java
@@ -0,0 +1,15 @@
+package com.psychic_engine.cmput301w17t10.feelsappman.Models;
+
+import org.osmdroid.util.GeoPoint;
+
+/**
+ * Created by Pierre Lin on 3/25/2017.
+ */
+
+public class Location {
+    private GeoPoint loc;
+
+    public void setLoc(GeoPoint loc) {
+        this.loc = loc;
+    }
+}
