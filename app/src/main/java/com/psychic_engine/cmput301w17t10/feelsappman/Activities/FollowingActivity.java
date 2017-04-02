39bf3b64cc11053066a8cd07c13c4cfbea0d8559
hnkhan <hnkhan@ualberta.ca>
2017-04-02 05:16:11 -0600

    Following updated

--- app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/Activities/FollowingActivity.java
+++ app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/Activities/FollowingActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,17 +14,23 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 
+import com.psychic_engine.cmput301w17t10.feelsappman.Controllers.ElasticParticipantController;
+import com.psychic_engine.cmput301w17t10.feelsappman.Models.Participant;
 import com.psychic_engine.cmput301w17t10.feelsappman.Models.ParticipantSingleton;
 import com.psychic_engine.cmput301w17t10.feelsappman.R;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class FollowingActivity extends AppCompatActivity {
     private ParticipantSingleton instance;
     private Spinner menuSpinner;
     private ListView followingList;
     private Button search;
+    private ArrayList<String> followingArray;
+    private ArrayAdapter<String> adapter;
+    private Participant participant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +45,24 @@ public class FollowingActivity extends AppCompatActivity {
         search = (Button) findViewById(R.id.search);
 
         followingList = (ListView) findViewById(R.id.listViewFollowing);
+        registerForContextMenu(followingList);
+
+        followingArray = new ArrayList<String>();
 
         initializeSpinner();
+
+        ElasticParticipantController.FindParticipantTask fpt = new ElasticParticipantController.FindParticipantTask();
+        fpt.execute(ParticipantSingleton.getInstance().getSelfParticipant().getLogin());
+
+        try {
+            participant = fpt.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        followingArray = participant.getFollowing();
 
         search.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +71,41 @@ public class FollowingActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    //http://stackoverflow.com/questions/17207366/creating-a-menu-after-a-long-click-event-on-a-list-view
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        if (v.getId() == R.id.listViewFollowing) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.menu_stopfollowing, menu);
+        }
+    }
+
+    //long click for editing and deleting
+    //http://stackoverflow.com/questions/17207366/creating-a-menu-after-a-long-click-event-on-a-list-view
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch(item.getItemId()) {
+            case R.id.stopfollowing:
+
+                setResult(RESULT_OK);
+                adapter.notifyDataSetChanged();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        adapter = new ArrayAdapter<String>(this, R.layout.follower_item, followingArray);
+        followingList.setAdapter(adapter);
     }
 
     public void initializeSpinner(){
