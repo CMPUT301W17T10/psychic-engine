a300c1e6e749ef56b972b6534c2c478e64e72643
hnkhan <hnkhan@ualberta.ca>
2017-04-02 05:45:29 -0600

    My Feed shows moodevents

--- app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/Activities/MyFeedActivity.java
+++ app/src/main/java/com/psychic_engine/cmput301w17t10/feelsappman/Activities/MyFeedActivity.java
@@ -10,16 +10,26 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 
+import com.psychic_engine.cmput301w17t10.feelsappman.Controllers.ElasticParticipantController;
+import com.psychic_engine.cmput301w17t10.feelsappman.Models.Mood;
+import com.psychic_engine.cmput301w17t10.feelsappman.Models.MoodEvent;
+import com.psychic_engine.cmput301w17t10.feelsappman.Models.Participant;
 import com.psychic_engine.cmput301w17t10.feelsappman.Models.ParticipantSingleton;
 import com.psychic_engine.cmput301w17t10.feelsappman.R;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MyFeedActivity extends AppCompatActivity {
     private ParticipantSingleton instance;
     private Spinner menuSpinner;
+    private ArrayList<MoodEvent> followingMoodsArray;
+    private ArrayList<String> followingArray;
     private ListView myFeedList;
+    private ArrayAdapter<MoodEvent> adapter;
+    private Participant participant;
+    private Participant following;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +44,49 @@ public class MyFeedActivity extends AppCompatActivity {
 
         myFeedList = (ListView) findViewById(R.id.listViewMyFeed);
 
+        followingMoodsArray = new ArrayList<MoodEvent>();
+        followingArray = new ArrayList<String>();
+
+        ElasticParticipantController.FindParticipantTask fpt = new ElasticParticipantController.FindParticipantTask();
+        fpt.execute(ParticipantSingleton.getInstance().getSelfParticipant().getLogin());
+
+        try {
+            participant = fpt.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        followingArray = participant.getFollowing();
+
+        for (String followerName : followingArray) {
+            ElasticParticipantController.FindParticipantTask fpt1 = new ElasticParticipantController.FindParticipantTask();
+            fpt1.execute(followerName);
+
+            try {
+                following = fpt1.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            for (MoodEvent moodEvent : following.getMoodList()) {
+                followingMoodsArray.add(moodEvent);
+            }
+        }
+
+
+
         initializeSpinner();
+    }
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        adapter = new ArrayAdapter<MoodEvent>(this, R.layout.myfeed_item, followingMoodsArray);
+        myFeedList.setAdapter(adapter);
     }
 
     public void initializeSpinner(){
